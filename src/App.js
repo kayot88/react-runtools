24a535d892bbedbc0dd7208492275af533c5a6e9
Kayot88 <kayot88@gmail.com>
2019-11-17 20:29:35 +0200

    Asynchronously Load webpack Bundles through Code-splitting and React Suspense

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
-import {hot} from 'react-hot-loader'
-import './style.css'
+import { hot } from "react-hot-loader";
+import "./style.css";
 
- class App extends Component {
+const Warning = React.lazy(() =>  import('./Warning') )
+
+class App extends Component {
   state = {
     count: 0
   };
   render() {
     return (
       <div>
-        <h1 className={this.state.count > 5 ? 'warning':null}>Hellou from react</h1>
+        <h1 className={this.state.count > 10 ? "warning" : null}>
+          Hellow from react
+        </h1>
         <h2>Count: {this.state.count}</h2>
         <button
           onClick={() => this.setState(state => ({ count: state.count + 1 }))}
@@ -21,9 +25,14 @@ import './style.css'
         >
           -
         </button>
+        {this.state.count > 10 ? 
+        <React.Suspense fallback={null}>
+          <Warning /> 
+        </React.Suspense>
+        :null}
       </div>
     );
   }
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,18 +15,25 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: [["@babel/preset-env", {
-            targets: [
-              'last 2 versions',
-              'not dead',
-              // 'not < 2%',
-              // 'not ie 11'
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: [
+                  "last 2 versions",
+                  "not dead"
+                  // 'not < 2%',
+                  // 'not ie 11'
+                ],
+                useBuiltIns: "entry"
+              }
             ],
-            useBuiltIns: 'entry'
-          }], "@babel/preset-react"],
+            "@babel/preset-react"
+          ],
           plugins: [
             "react-hot-loader/babel",
             "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-syntax-dynamic-import"
           ]
         }
       },
